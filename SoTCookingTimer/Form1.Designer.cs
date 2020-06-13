d32b3c0cbb0a4bce954500ff51b8fef7b9f22818
Bregann <35966588+Bregann@users.noreply.github.com>
2020-06-13 13:15:46 +0100

    Add project files.

---
+++ SoTCookingTimer/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SoTCookingTimer
+{
+    partial class cookingTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.foodType = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timeLeftLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(188)))), ((int)(((byte)(250)))));
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(658, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sea of Thieves banging cooking timer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Jokerman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(15, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Type of food cooking:";
+            // 
+            // foodType
+            // 
+            this.foodType.AutoSize = true;
+            this.foodType.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foodType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.foodType.Location = new System.Drawing.Point(257, 76);
+            this.foodType.Name = "foodType";
+            this.foodType.Size = new System.Drawing.Size(80, 30);
+            this.foodType.TabIndex = 2;
+            this.foodType.Text = "None";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Snap ITC", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 45);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Time left:";
+            // 
+            // timeLeftLabel
+            // 
+            this.timeLeftLabel.AutoSize = true;
+            this.timeLeftLabel.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeftLabel.ForeColor = System.Drawing.Color.Red;
+            this.timeLeftLabel.Location = new System.Drawing.Point(238, 125);
+            this.timeLeftLabel.Name = "timeLeftLabel";
+            this.timeLeftLabel.Size = new System.Drawing.Size(64, 29);
+            this.timeLeftLabel.TabIndex = 4;
+            this.timeLeftLabel.Text = "None";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(468, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Button Key";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(468, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "F9 = Fish";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(469, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "F10 = Trophy Fish";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(469, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(179, 18);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "F11 = Meat and Shark";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(469, 143);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(159, 18);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "F12 = Meg and Krak";
+            // 
+            // cookingTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 164);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timeLeftLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.foodType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "cookingTimer";
+            this.Text = "SoT cooking timer";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.cookingTimer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label foodType;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label timeLeftLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ SoTCookingTimer/Form1.cs
@@ -0,0 +1,119 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoTCookingTimer
+{
+    public partial class cookingTimer : Form
+    {
+        private int _timerSeconds;
+        private Timer _timer;
+        private IKeyboardMouseEvents _keyboardEvents;
+
+        public cookingTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void cookingTimer_Load(object sender, EventArgs e)
+        {
+            _keyboardEvents = Hook.GlobalEvents();
+
+            var fkeys = new Dictionary<Combination, Action>();
+            fkeys.Add(Combination.TriggeredBy(Keys.F9), F9Press); //Fish
+            fkeys.Add(Combination.TriggeredBy(Keys.F10), F10Press); //Trophy Fish
+            fkeys.Add(Combination.TriggeredBy(Keys.F11), F11Press); //Meat & shark
+            fkeys.Add(Combination.TriggeredBy(Keys.F12), F12Press); //Meg & Kraken
+
+            _keyboardEvents.OnCombination(fkeys);
+        }
+
+        private void F9Press()
+        {
+            if (_timer != null)
+            {
+                _timer.Dispose();
+            }
+
+            _timerSeconds = 45;
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += TimerTick;
+            _timer.Start();
+            foodType.Text = "Normie Fish";
+            timeLeftLabel.Text = $"{_timerSeconds} seconds my dude";
+        }
+
+        private void F10Press()
+        {
+            if (_timer != null)
+            {
+                _timer.Dispose();
+            }
+
+            _timerSeconds = 90;
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += TimerTick;
+            _timer.Start();
+            foodType.Text = "Trophy fish";
+            timeLeftLabel.Text = $"{_timerSeconds} seconds my dude";
+        }
+
+        private void F11Press()
+        {
+            if (_timer != null)
+            {
+                _timer.Dispose();
+            }
+
+            _timerSeconds = 60;
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += TimerTick;
+            _timer.Start();
+            foodType.Text = "Hot and spicy meat";
+            timeLeftLabel.Text = $"{_timerSeconds} seconds my dude";
+        }
+
+        private void F12Press()
+        {
+            if (_timer != null)
+            {
+                _timer.Dispose();
+            }
+
+            _timerSeconds = 120;
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += TimerTick;
+            _timer.Start();
+            foodType.Text = "Meg or Kraken";
+            timeLeftLabel.Text = $"{_timerSeconds} seconds my dude";
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (_timerSeconds == 0)
+            {
+                SoundPlayer player = new SoundPlayer("SoundEffect/food.wav");
+                player.Play();
+                _timer.Dispose();
+                return;
+            }
+
+            _timerSeconds--;
+
+            timeLeftLabel.Text = $"{_timerSeconds} seconds my dude";
+
+        }
+    }
+}
